ae032d8710799960c287f8ad77774e078b838467
Alex Zaharow <alex-zaharow@yandex.ru>
2014-02-09 15:47:26 +0400

    init

---
+++ src/main/java/ru/satabol/javaApp02/App.java
@@ -0,0 +1,13 @@
+package ru.satabol.javaApp02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
