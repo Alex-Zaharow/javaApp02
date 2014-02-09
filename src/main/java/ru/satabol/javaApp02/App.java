bdeadc99b72ebf10da6b65469388b3dfd575d552
Alex Zaharow <alex-zaharow@yandex.ru>
2014-02-09 19:20:22 +0400

    0.0.5

--- src/main/java/ru/satabol/javaApp02/App.java
+++ src/main/java/ru/satabol/javaApp02/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hello release 0.0.4!" );
+        System.out.println( "hello release 0.0.5!" );
     }
 }
